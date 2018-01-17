18fc8cab3acf0659795cb38067fce0a118a7b423
simsingh293 <sim.singh993@gmail.com>
2018-01-17 04:10:42 +0000

    Completed Blog App

--- RESTful_Routing/BlogApp/index.js
+++ RESTful_Routing/BlogApp/index.js
@@ -1,17 +1,20 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 var app = express();
 
 
 // CONFIGURATIONS
-
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-// MODEL CONFIGURATIONS
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
+// MODEL CONFIGURATIONS
 var blogSchema = new mongoose.Schema({
     title: String,
     image: {type: String, default: "http://maxpixel.freegreatpicture.com/static/photo/1x/Nature-Young-Eyes-Lion-Close-Juvenile-Wildlife-2808134.jpg"},
@@ -27,10 +30,12 @@ var Blog = mongoose.model("Blog", blogSchema);
 //     body: "This is a test, but that bridge looks pretty sick."
 // })
 
+//ROOT ROUTE
 app.get("/", function(req, res){
     res.redirect("/blogs");
 })
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -41,7 +46,67 @@ app.get("/blogs", function(req, res){
     });
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
 
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+    req.body.blog.body  = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            res.redirect("/blogs")
+        }
+    });
+});
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body  = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
--- RESTful_Routing/readme.md
+++ RESTful_Routing/readme.md
@@ -16,3 +16,31 @@ DESTROY     /destroyBlog/:id
 * Create the blog model
 * Add INDEX route and template
 * Add simple nav bar
+
+#Putting the C in CRUD
+* Add NEW route
+* Add NEW template
+* Add CREATE route
+* Add CREATE template
+
+#Creating the SHOW route
+* Add SHOW route
+* Add SHOW template
+* Add links to show page
+* Style show template
+
+#EDIT/UPDATE
+* Add EDIT route
+* Add EDIT form
+* Add UPDATE route
+* Add UPDATE form
+* Add Method-Override
+
+#DESTROY ROUTE
+* Add DESTROY route
+* Add EDIT and DESTROY links
+
+#FINAL UPDATES
+* Sanitize blog body
+* Style INDEX
+* Update REST Table
